f0a4ba6f05d9d1f6e4270833a0f7c88d6048e68c
Axton <axton@arizon.dev>
2024-02-12 05:57:40 +0000

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,28 +1,16 @@
-#!/bin/ash
-
 # $1 = {SERVER_MEMORY}
 # $2 = {MINECRAFT_VERSION}
 
 # Determine version
 url='https://papermc.io/api/v2/projects/paper'
 version=$2
-
-if [ "$version" = "latest" ]; then
-    version=$(curl -sSfL "$url")
-    version=${version%\"*}
-    version=${version##*\"}
-fi
-
+if [ "$version" = "latest" ]; then version=$(curl -sSfL "$url"); version=${version%\"*} version=${version##*\"}; fi
 echo "[Arizon Software] Updating PaperMC in '/paper/$version/paper-$version.jar'..."
-build=$(curl -sSfL "$url/versions/$version")
-build=${build%]*}
-build=${build##*[,[]}
+build=$(curl -sSfL "$url/versions/$version"); build=${build%]*} build=${build##*[,[]}
 echo "[Arizon Software] Resolved latest version as $version (build #$build)"
 
-# Create version folder
-mkdir -p "/home/container/paper/$version/"
-
 # Download latest version
+mkdir -p "/home/container/paper/$version/"
 jarPath="/home/container/paper/$version/paper-$version.jar"
 curl -sSfL "$url/versions/$version/builds/$build/downloads/paper-$version-$build.jar" -o "$jarPath"
 echo "[Arizon Software] Successfully updated PaperMC to build #$build (MC $version)"
