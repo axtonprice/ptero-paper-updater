d74f8830bd0beb1e5edaf42583d116555e1147fb
Axton <axton@arizon.dev>
2024-02-12 06:07:58 +0000

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,20 +1,17 @@
-# $1 = {SERVER_MEMORY}
-# $2 = {MINECRAFT_VERSION}
-
-# Determine version
+#$1 = {SERVER_MEMORY}
+#$2 = {MINECRAFT_VERSION}
+#Determine version
 url='https://papermc.io/api/v2/projects/paper'
 version=$2
 if [ "$version" = "latest" ]; then version=$(curl -sSfL "$url"); version=${version%\"*} version=${version##*\"}; fi
 echo "[Arizon Software] Updating PaperMC in '/paper/$version/paper-$version.jar'..."
 build=$(curl -sSfL "$url/versions/$version"); build=${build%]*} build=${build##*[,[]}
 echo "[Arizon Software] Resolved latest version as $version (build #$build)"
-
-# Download latest version
+#Download latest version
 mkdir -p "/home/container/paper/$version/"
 jarPath="/home/container/paper/$version/paper-$version.jar"
 curl -sSfL "$url/versions/$version/builds/$build/downloads/paper-$version-$build.jar" -o "$jarPath"
 echo "[Arizon Software] Successfully updated PaperMC to build #$build (MC $version)"
-
-# Start server
+#Start server
 echo "[Arizon Software] Starting server..."
-java -Xms256M -Xmx"$1"M -jar "$jarPath" -com.mojang.eula.agree=true
+java -Xms256M -Xmx"$1"M -jar "$jarPath" -Dcom.mojang.eula.agree=true
